e5eceb919efb9358f0f93bd77f65a55ca911e82f
Mudasir Kakepoto <muddasirabro724@gmail.com>
2024-10-20 14:22:23 +0500

    new feature added

---
+++ app.js
@@ -0,0 +1,28 @@
+const input=document.getElementById('birthday')
+const btn=document.getElementById('btn')
+const res=document.getElementById('result')
+const name=document.getElementById('name')
+function calculateAge(){
+    const value=input.value;
+    const nameValue=name.value;
+    if(value===""){
+        alert('Enter your age........')
+    }else{
+        const age=getAge(value)
+        res.textContent=`${nameValue}-You are ${age}${age>1?"years":"year"} old.`
+        input.value=''
+        name.value=''
+    }
+}
+function getAge(val){
+    const currentDate=new Date()
+    const BirthdayDate=new Date(val)
+    let age=currentDate.getFullYear()-BirthdayDate.getFullYear()
+    const month=currentDate.getMonth()-BirthdayDate.getMonth()
+    if(month<0 || (month===0 && currentDate.getDate()<BirthdayDate.getDate())){
+        age--
+    }else{
+        return age
+    }
+}
+btn.addEventListener('click',calculateAge)
